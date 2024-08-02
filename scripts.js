914c1af1b7fcf012361f53d14ce0146c00d8b34e
RafalLyszczak <119815400+RafalLyszczak@users.noreply.github.com>
2024-08-02 14:18:01 +0100

    Update scripts.js
    Final

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function changeText1(){
     newDiv.id = "main_text";
     newDiv.innerHTML = `
     <p>
-        Hi, welcome to my portfolio web project. I would like to give you a quick few on my person.
+        Hi, welcome to my portfolio web project. I would like to give you a quick view on my person.
     </p> 
     <p>
         I am a Polish man who moved to the UK in October 2020, just before the COVID-19 lockdowns. IT, physiq, food, mecial freak.
@@ -16,10 +16,10 @@ function changeText1(){
             My first interaction with IT/Code was at school when I was 16. I found the code really interesting but never worked professionally with it.
         </li>
         <li>
-            Sport is quite a new hobby for me (3 years), as is mobility and building muscles. My favorite part of that is progression; I don't like being stuck in one place.
+            Sport is quite a new hobby for me (3 years), as is mobility and building muscles. My favorite part of that is progression.
         </li>
         <li>
-            I love preaching food, especially desserts. My meal sorry started when I was 10. From small meals for myself to making parties for 14 people, traditional food is the key.
+            I love preaching food, especially desserts. My meal story started when I was 10. From small meals for myself to making parties for 14 people, traditional food is the key.
         </li>
         <li>
             My whole life, I was into the body; there was never anything weird or odd about humans. My medical career gives me the opportunity to explore even more.
@@ -40,7 +40,7 @@ function changeText2(){
     newDiv.id = "main_text";
     newDiv.innerHTML = `
     <p>
-        I obtain my Education in Poland, details below:<br>
+        I obtained my Education in Poland, details below:<br>
     </p>
     <ul>
         <li>
@@ -70,14 +70,14 @@ function changeText3(){
     newDiv.id = "main_text";
     newDiv.innerHTML = `
     <p>
-        I do not have professional work experience in coding or other IT departments.<br> My history of employment:
+        I do not have professional work experience in codding or other IT departments.<br> My history of employment:
     </p>
     <ul>
         <li>
             Technician (school apprenticeship) | Internet Provider Company 01/05/2019–01/07/2019 It was my mandatory apprenticeship for my education to get qualification. I had to work for 2 months in a different place than I used to live. I spent some time working with routers, graphic programs, the marketing team (which we had to teach graphic editing), and customer service.
         </li>
         <li>
-            Carrer | Self-employed 01/01/2021–01/02/2023: Private care for an elderly lady and her family. This job I got accidentally. My firend elderly lady needed some help, and from time to time I helped her, and she offered me to become her primary caregiver.
+            Carrer | Self-employed 01/01/2021–01/02/2023: Private care for an elderly lady and her family. This job I got accidentally. My firend eneeded some help, and from time to time I helped her, and she offered me to become her primary caregiver.
         </li>
         <li>
             HCA | NHS31/05/2023 – CURRENT Deliver high-quality care to patients and their families. In this one, I learned a lot about hospital work, how tons of data is transferred and confidently stored, medical abbreviation, working under pressure, and moving around new programs.
@@ -95,14 +95,14 @@ function changeText4(){
     newDiv.id = "main_text";
     newDiv.innerHTML = `
     <p>
-        I started to code in the past, but as a hobby, then took quite a long break. I am definitely into code. Currently, I am working in the hospital as an HCA. I was comparing nursing to codding, and I realized I like codding more than nursing. The main reason is that computers and code are more familiar with my personality.
+        I started to code in the past, but as a hobby, then took quite a long break. I am definitely into code. Currently, I am working in the hospital as an HCA. I was comparing nursing to coding, and I realized I like coding more than nursing. The main reason is that computers and code are more familiar with my personality.
     </p>
     <h2>
         Goals:
     </h2>
     <ul>
         <li>
-            Start carreer as front end developer (apprenticeship/entry posision/inter position)</li><li>Get know cod in professional way,
+            Start career as front end developer (apprenticeship/entry position/inter position)</li><li>Get know code in professional way,
         </li>
         <li>
             Extend my skills to full-stack web developer,
