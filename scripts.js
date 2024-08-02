b67ccd84f7ba2f8225a93dc371b192d82d45ff88
RafalLyszczak <119815400+RafalLyszczak@users.noreply.github.com>
2024-08-02 13:44:11 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,30 @@ function changeText1(){
     var originalDiv = document.getElementById('main_text');
     var newDiv = document.createElement('div');
     newDiv.id = "main_text";
-    newDiv.innerHTML = '<p>Hi welcome to my porfolio web project. I would like to give you quick few on my person.</p><p><p>I am polish guy, who moved to uk in October 2020 just beofre COVID-19 Lockdowns.IT, physiq, food, mecial freak.</p><ul id="ulelement"><li>First contact with IT/Code had at school when was 16, find out code realy intresting, but never wokred profesional way with it.</li><li>Sport is quite new hobby for me (3 years), mobility, buidling muscles. My favoure part of that is progression, dont like stuck in one place.</li><li>Love preapring food specially dessets, my meal sorry started since i was 10. From small meals for my-self to making parties for 14 people. Traditional food is the key. </li><li>My whole life i was into body, there was never anhthing weird or odd in human. My medical career ghave me opportnity to expolore even more.</li></ul><p>My life goal is going foward and exploring new things, and self-development specialy education. </p>';
+    newDiv.innerHTML = `
+    <p>
+        Hi, welcome to my portfolio web project. I would like to give you a quick few on my person.
+    </p> 
+    <p>
+        I am a Polish man who moved to the UK in October 2020, just before the COVID-19 lockdowns. IT, physiq, food, mecial freak.
+    </p>
+    <ul>
+        <li>
+            My first interaction with IT/Code was at school when I was 16. I found the code really interesting but never worked professionally with it.
+        </li>
+        <li>
+            Sport is quite a new hobby for me (3 years), as is mobility and building muscles. My favorite part of that is progression; I don't like being stuck in one place.
+        </li>
+        <li>
+            I love preaching food, especially desserts. My meal sorry started when I was 10. From small meals for myself to making parties for 14 people, traditional food is the key.
+        </li>
+        <li>
+            My whole life, I was into the body; there was never anything weird or odd about humans. My medical career gives me the opportunity to explore even more.
+        </li>
+    </ul>
+    <p>
+        My life goal is going forward, exploring new things, and self-development, especially in education.
+    </p>`;
     
     container.replaceChild(newDiv, originalDiv);
 
@@ -15,7 +38,26 @@ function changeText2(){
     var originalDiv = document.getElementById('main_text');
     var newDiv = document.createElement('div');
     newDiv.id = "main_text";
-    newDiv.innerHTML = "<p>I obtain my Education in Poland, details below:<br></p><ul><li>Zs1 in Hrubieszow - IT specialist | technician school (IB / BTEC) -2020 - equivalent o english A-levels with specialization  IT(),</li><li>RQF Level 3 / SCQF Level 6-7 / CQFW Level 3 IT - 2020,</li><li>Graphic designer [VCC +Europass] – 2019 - Program foundated by union to eduate young ambitious people in Schools.,</li><li>Data administrator [VCC +Europass] – 2019 - Program foundated by union to eduate young ambitious people in Schools.,</li><li>HCA [NHS] - 2023 - Certificate with cooperation with NHS in Royal Surrey Trust Hospital in Eduacion center and on wards.</li></ul>";
+    newDiv.innerHTML = `
+    <p>
+        I obtain my Education in Poland, details below:<br>
+    </p>
+    <ul>
+        <li>
+            Zs1 in Hrubieszow - IT Specialist | Technician School (IB / BTEC) 2020-Equivalent English A-Levels with Specialization IT(),
+        </li>
+        <li>
+            RQF Level 3/SSCQF Level 6-7/CCQFW Level 3 IT - 2020,</li>
+        <li>
+            Graphic designer [VCC + Europass] 2019: A program founded by the union to educate young, ambitious people in schools.
+        </li>
+        <li>
+            Data Administrator [VCC + Europass]—2019—Program founded by the union to educate young, ambitious people in schools.
+        </li>
+        <li>
+            HCA [NHS] 2023: Certificate with cooperation with the NHS in Royal Surrey Trust Hospital in Eduacion Center and onwards.
+        </li>
+    </ul>`;
     
     container.replaceChild(newDiv, originalDiv);
 
@@ -26,7 +68,21 @@ function changeText3(){
     var originalDiv = document.getElementById('main_text');
     var newDiv = document.createElement('div');
     newDiv.id = "main_text";
-    newDiv.innerHTML = "<p>I do not have big work eperience in coding or other It departments.<br>My Hisory of employment:</p><ul><li>Technician (school apprenticeship) | Internet Provider Company 01/05/2019 – 01/07/2019 - Was my mandatory apprenticeship for my eduaction to get qualifacion. Had work for 2 months in diffrent place than I used to live my spend some time work with routers, graphic programs, margeting team (which we had to teach graphic editing) and Customers Service</li><li>Carrer | Self-employed01/01/2021 – 01/02/2023 - Private care for an elderly lady and her family. This job i get accidentally. My firend eldery lady neded some help times to times i help her and she offered me to become her priave carrer</li><li>HCA | NHS31/05/2023 – CURRENT Deliver high quality care to patients and their families. In this one i learned alot how to Hospital work and tons of data is transfered and confidentaly stored, medical abbroviation, wiking under the pressure, and move around new programs</li></ul>";
+    newDiv.innerHTML = `
+    <p>
+        I do not have professional work experience in coding or other IT departments.<br> My history of employment:
+    </p>
+    <ul>
+        <li>
+            Technician (school apprenticeship) | Internet Provider Company 01/05/2019–01/07/2019 It was my mandatory apprenticeship for my education to get qualification. I had to work for 2 months in a different place than I used to live. I spent some time working with routers, graphic programs, the marketing team (which we had to teach graphic editing), and customer service.
+        </li>
+        <li>
+            Carrer | Self-employed 01/01/2021–01/02/2023: Private care for an elderly lady and her family. This job I got accidentally. My firend elderly lady needed some help, and from time to time I helped her, and she offered me to become her primary caregiver.
+        </li>
+        <li>
+            HCA | NHS31/05/2023 – CURRENT Deliver high-quality care to patients and their families. In this one, I learned a lot about hospital work, how tons of data is transferred and confidently stored, medical abbreviation, working under pressure, and moving around new programs.
+        </li>
+    </ul>`;
     
     container.replaceChild(newDiv, originalDiv);
 
@@ -37,7 +93,21 @@ function changeText4(){
     var originalDiv = document.getElementById('main_text');
     var newDiv = document.createElement('div');
     newDiv.id = "main_text";
-    newDiv.innerHTML = "<p>I started code in the past but as hobby, then had quite long break. I am defnietly into code. Currently I am working in hospital as HCA. I was comparning Nursing to codding, and i realize i enoy codding more than Nursing. Main reason is Computers and code is more familiar with my personality.</p><h2>Goals:</h2><ul><li>Start carreer as front end developer (apprenticeship/entry posision/inter position)</li><li>Get know cod in professional way</li><li>Extend my skills to full stack web developer</li></ul>";
+    newDiv.innerHTML = `
+    <p>
+        I started to code in the past, but as a hobby, then took quite a long break. I am definitely into code. Currently, I am working in the hospital as an HCA. I was comparing nursing to codding, and I realized I like codding more than nursing. The main reason is that computers and code are more familiar with my personality.
+    </p>
+    <h2>
+        Goals:
+    </h2>
+    <ul>
+        <li>
+            Start carreer as front end developer (apprenticeship/entry posision/inter position)</li><li>Get know cod in professional way,
+        </li>
+        <li>
+            Extend my skills to full-stack web developer,
+        </li>
+    </ul>`;
     
     container.replaceChild(newDiv, originalDiv);
 
